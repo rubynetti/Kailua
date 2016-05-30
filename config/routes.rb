41baceb394da2de3c1320bade9a61b6ffce8948b
Alessandro <descovi@gmail.com>
2016-05-30 12:17:05 +0200

    routing to socios

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :socios
-  resources :socis
-  root "socis#index"
+  root "socios#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
