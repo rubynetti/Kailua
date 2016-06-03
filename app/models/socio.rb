c924730ce5feea38c639fe54e5abda457672118d
Alessandro <descovi@gmail.com>
2016-06-03 17:00:11 +0200

    verde se elenco completo a disposizione

--- app/models/socio.rb
+++ app/models/socio.rb
@@ -1,3 +1,9 @@
 class Socio < ApplicationRecord
   validates :number, uniqueness: true, allow_blank: true
+  def completo?
+    if number.blank? or cf.blank? or name.blank? or surname.blank? or contact.blank? or complete == false or birthdate.blank?
+      return false 
+    end
+    return true
+  end
 end
