ce3e8664d9b337b91dc55574c9fdaa1c10e67ad0
Alessandro <descovi@gmail.com>
2016-06-03 17:42:26 +0200

    complete have to have more data

--- app/models/socio.rb
+++ app/models/socio.rb
@@ -1,7 +1,7 @@
 class Socio < ApplicationRecord
   validates :number, uniqueness: true, allow_blank: true
   def completo?
-    if number.blank? or cf.blank? or name.blank? or surname.blank? or contact.blank? or complete == false or birthdate.blank?
+    if number.blank? or cf.blank? or name.blank? or surname.blank? or contact.blank? or complete == false or birthdate.blank? or residence_place.blank?
       return false 
     end
     return true
