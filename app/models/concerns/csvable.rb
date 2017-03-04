6512189f920c67fd6416568f5ea00aec7e3b9ab9
Alessandro <descovi@gmail.com>
2017-03-04 13:13:47 +0100

    riordina l'aspetto di csvable

--- app/models/concerns/csvable.rb
+++ app/models/concerns/csvable.rb
@@ -1,18 +1,21 @@
 require 'active_support/concern'
 
 module Csvable
-  extend ActiveSupport::Concern
-    included do
 
-def self.to_csv(options = {})
-    CSV.generate(options) do |csv|
-      csv << column_names
-      all.each do |socio|
-        row = socio.attributes.values_at(*column_names)
-        csv << row
-      end
-    end
-  end
-  end
+  extend ActiveSupport::Concern
   
+  included do
+
+    def self.to_csv(options = {})
+        CSV.generate(options) do |csv|
+          csv << column_names
+          all.each do |socio|
+            row = socio.attributes.values_at(*column_names)
+            csv << row
+          end
+        end
+    end
+
+  end
+
 end
